b22a56f2a054bd799db4a184662c1f20d6a7cfb6
MARIN4444 <gohan44marin@gmail.com>
2025-04-09 01:42:22 -0500

    implement forwardTraversal and backwardTraversak methods

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -334,25 +334,25 @@ class TestDoublyLinkedList(unittest.TestCase):
         self.assertEqual(self.dll.head.data, 1)
         self.assertEqual(self.dll.tail.data, 4)
     
-    def test_forwardTraversal(self):
+    def test_forward_traversal(self):
         """Test forward traversal"""
-        self.assertEqual(self.dll.forwardTraversal(), [])
+        self.assertEqual(self.dll.forward_traversal(), [])
         
         self.dll.append(1)
         self.dll.append(2)
         self.dll.append(3)
         
-        self.assertEqual(self.dll.forwardTraversal(), [1, 2, 3])
+        self.assertEqual(self.dll.forward_traversal(), [1, 2, 3])
     
-    def test_backwardTraversal(self):
+    def test_backward_traversal(self):
         """Test backward traversal"""
-        self.assertEqual(self.dll.backwardTraversal(), [])
+        self.assertEqual(self.dll.backward_traversal(), [])
         
         self.dll.append(1)
         self.dll.append(2)
         self.dll.append(3)
         
-        self.assertEqual(self.dll.backwardTraversal(), [3, 2, 1])
+        self.assertEqual(self.dll.backward_traversal(), [3, 2, 1])
 
 if __name__ == '__main__':
     unittest.main()
